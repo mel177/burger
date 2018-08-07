1b97454bc592762a786eff19eaab8c1833f34000
Melinh Mai <melinh.mai@gmail.com>
2018-08-07 00:12:41 -0500

    updates

--- controller/routes.js
+++ controller/routes.js
@@ -6,8 +6,8 @@ router.get('/', function(req,res){
     burger.all(function(burger_data){
         console.log(burger_data);
         res.render('index',{burger_data});
-    })
-})
+    });
+});
 
 router.put('/burgers/update',function(req,res){
     burger.update(req.body.burger_id, function(result){
